d98caedf7687aa35bd285c717fda8722c4a22a9e
Victor Smeets <victorsmeets.vs@gmail.com>
2023-11-25 22:31:21 +0100

    init

---
+++ accelerometer_app/README.md
@@ -0,0 +1,3 @@
+# accelerometer_app
+
+A new Flutter project.
---
+++ accelerometer_app/android/app/src/main/kotlin/com/example/accelerometer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.accelerometer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
